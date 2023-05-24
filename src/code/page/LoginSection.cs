a0d3e7bf317d235554ef43b568eef012c83f3949
MarcoPoma <40254356+MarcoPoma@users.noreply.github.com>
2023-05-23 21:47:15 -0400

    Add files via upload

---
+++ src/code/page/LoginSection.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using SeleniumTraining.src.code.control;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumTraining.src.code.page
+{
+    public class LoginSection
+    {
+        public TextBox emailTxtBox = new TextBox(By.ClassName("ycptinput"));
+        
+        public Button loginButton = new Button(By.Id("refreshbut"));
+
+        public Button newemail = new Button(By.Id("newmail"));
+
+
+       
+        public TextBox pwdTxtBox = new TextBox(By.CssSelector("div.tooltip.click input#msgto"));
+        
+        public TextBox asuntoInput = new TextBox(By.CssSelector("div.inputsend input#msgsubject"));
+      
+        public TextBox cuerpoMensajeInput = new TextBox(By.CssSelector("div#msgbody"));
+        
+        public Button sendButton = new Button(By.CssSelector("button#msgsend"));
+        
+
+        public void Login(String user, String pwd,String asunto, String cuerpo) 
+        { 
+            emailTxtBox.SetText(user);
+            pwdTxtBox.SetText(pwd);
+            asuntoInput.SetText(asunto);
+            cuerpoMensajeInput.SetText(cuerpo);
+            loginButton.Click();
+        }
+    }
+}
---
+++ src/code/test/LoginTest.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using SeleniumTraining.src.code.page;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumTraining.src.code.test
+{
+    [TestClass]
+    public class LoginTest : TestBase
+    {
+
+        MainPage mainPage = new MainPage();
+        LoginSection loginSection = new LoginSection();
+        MenuSection menuSection = new MenuSection();
+
+
+
+        [TestMethod]
+        public void VerifyTheLoginIsSuccessfuly2()
+        {
+            //mainPage.loginButton.Click();
+            loginSection.emailTxtBox.SetText("asd");
+            //loginSection.pwdTxtBox.SetText("12345");
+            loginSection.loginButton.Click();
+            loginSection.newemail.Click();
+            loginSection.pwdTxtBox.SetText("marcopoma943@gmail.com");
+            loginSection.asuntoInput.SetText("Asunto del mensaje");
+            loginSection.cuerpoMensajeInput.SetText("Contenido del mensaje");
+            loginSection.sendButton.Click();
+            Assert.IsTrue(menuSection.logoutButton.IsControlDisplayed(),
+                "ERROR !! the login was not successfully, review credentials please");
+        }
+
+    }
+}
